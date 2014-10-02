d83a506f7b1a7e5ca2de2bdf12e1096f4f986e0a
NetPuter <netputer@gmail.com>
2014-10-02 14:31:50 +0800

    chore: No test case needed

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,18 +17,12 @@ module.exports = function (grunt) {
     jshint: {
       all: [
         'Gruntfile.js',
-        'tasks/*.js',
-        '<%= nodeunit.tests %>'
+        'tasks/*.js'
       ],
       options: {
         jshintrc: '.jshintrc',
         reporter: require('jshint-stylish')
       }
-    },
-
-    // Before generating any new files, remove any previously-created files.
-    clean: {
-      tests: ['tmp']
     },
 
     // Configuration to be run (and then tested).
@@ -37,35 +31,21 @@ module.exports = function (grunt) {
         options: {
         },
         files: {
-          'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123']
         }
       },
       custom_options: {
         options: {
-          separator: ': ',
-          punctuation: ' !!!'
         },
         files: {
-          'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123']
         }
       }
-    },
-
-    // Unit tests.
-    nodeunit: {
-      tests: ['test/*_test.js']
     }
-
   });
 
   // Actually load this plugin's task(s).
   grunt.loadTasks('tasks');
 
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'adb_tools', 'nodeunit']);
-
-  // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint', 'test']);
+  // By default, lint and run this plugin's task(s).
+  grunt.registerTask('default', ['jshint', 'adb_tools']);
 
 };
