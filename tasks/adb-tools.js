659b7540a000819af0e6aaadddfc68ee17fb2eeb
NetPuter <netputer@gmail.com>
2014-10-07 14:37:11 +0800

    chore: Code style polish

--- tasks/adb-tools.js
+++ tasks/adb-tools.js
@@ -12,9 +12,6 @@ var exec = require('child_process').exec;
 
 module.exports = function (grunt) {
 
-    // Please see the Grunt documentation for more information regarding task
-    // creation: http://gruntjs.com/creating-tasks
-
     var run = function (command, callback) {
         exec(command, function (err, stdout, stderr) {
             grunt.log.ok('Command:', command);
@@ -31,16 +28,15 @@ module.exports = function (grunt) {
         });
     };
 
-    grunt.registerMultiTask('adbInstall', 'Pushes an Android application (specified as a full path to an .apk file) to an emulator/device.', function () {
+    grunt.registerMultiTask('adbInstall', 'Pushes an Android application to an emulator/device.', function () {
 
         var done = this.async();
-        var completeFiles = 0;
+        var completeCount = 0;
         var files = this.files;
 
-        // Iterate over all specified file groups.
         files.forEach(function (file) {
             run('adb install ' + file.src, function () {
-                if (++completeFiles === files.length) {
+                if (++completeCount === files.length) {
                     done();
                 }
             });
@@ -51,14 +47,12 @@ module.exports = function (grunt) {
     grunt.registerMultiTask('adbUninstall', 'Removes a package from the system.', function () {
 
         var done = this.async();
-        var completeFiles = 0;
-
+        var completeCount = 0;
         var packageNames = this.data.packageNames;
 
-        // Iterate over all specified file groups.
         packageNames.forEach(function (packageName) {
             run('adb uninstall ' + packageName, function () {
-                if (++completeFiles === packageNames.length) {
+                if (++completeCount === packageNames.length) {
                     done();
                 }
             });
@@ -69,13 +63,12 @@ module.exports = function (grunt) {
     grunt.registerMultiTask('adbPush', 'Copies a specified file from your development computer to an emulator/device instance.', function () {
 
         var done = this.async();
-        var completeFiles = 0;
+        var completeCount = 0;
         var files = this.files;
 
-        // Iterate over all specified file groups.
         files.forEach(function (file) {
             run('adb push ' + file.src + ' ' + file.dest, function () {
-                if (++completeFiles === files.length) {
+                if (++completeCount === files.length) {
                     done();
                 }
             });
@@ -86,14 +79,12 @@ module.exports = function (grunt) {
     grunt.registerMultiTask('adbStart', 'Start an Activity specified by intent.', function () {
 
         var done = this.async();
-        var completeFiles = 0;
-
+        var completeCount = 0;
         var intents = this.data.intents;
 
-        // Iterate over all specified file groups.
         intents.forEach(function (intent) {
             run('adb shell am start ' + intent, function () {
-                if (++completeFiles === intents.length) {
+                if (++completeCount === intents.length) {
                     done();
                 }
             });
@@ -104,14 +95,12 @@ module.exports = function (grunt) {
     grunt.registerMultiTask('adbForceStop', 'Force stop everything associated with package name.', function () {
 
         var done = this.async();
-        var completeFiles = 0;
-
+        var completeCount = 0;
         var packageNames = this.data.packageNames;
 
-        // Iterate over all specified file groups.
         packageNames.forEach(function (packageName) {
             run('adb shell am force-stop ' + packageName, function () {
-                if (++completeFiles === packageNames.length) {
+                if (++completeCount === packageNames.length) {
                     done();
                 }
             });
